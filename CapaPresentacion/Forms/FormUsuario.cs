ee6a77b70223eb5f2def098d2e40896b313559c7
Yunior Alex <53721642+YuniorAlex@users.noreply.github.com>
2020-10-07 23:21:49 -0500

    avansado hasta donde pude xd

--- CapaPresentacion/Forms/FormUsuario.cs
+++ CapaPresentacion/Forms/FormUsuario.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocios.Models;
+using CapaNegocios.ValueObjects;
 
 namespace CapaPresentacion.Forms
 {
     public partial class FormUsuario : Form
     {
+        private busuarioModel busuario = new busuarioModel();
         public FormUsuario()
         {
             InitializeComponent();
+        }
+
+        private void FormUsuario_Load(object sender, EventArgs e)
+        {
+            Listbusurio();
+        }
+        private void Listbusurio()
+        {
+            try
+            {
+                dataGridView1.DataSource = busuario.GetAll();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
