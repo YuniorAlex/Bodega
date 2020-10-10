dcb0b63e5fd620eca740acfb60dfe861ad658bec
Yunior Alex <53721642+YuniorAlex@users.noreply.github.com>
2020-10-10 00:08:38 -0500

    sdfsdf

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -17,7 +17,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Forms.FormPrincipal());
+            Application.Run(new Forms.FormInicioSesion());
         }
     }
 }
